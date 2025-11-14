e48ffe5dd33112eb53448823905d321a80dfaf31
annmarry01234 <annmaryglarance@gmail.com>
2025-11-14 17:12:50 +0530

    Add files via upload

---
+++ sum.ts
@@ -0,0 +1,6 @@
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Add a test call to see output
+console.log("2 + 3 =", add(2, 3));
---
+++ sum1.test.ts
@@ -0,0 +1,6 @@
+import {expect} from "chai";
+import {add} from "./sum";
+
+describe ("add function", ()=>{
+  it("should be",()=>{  expect(add(2,3)).to.equal(5);
+}); });
