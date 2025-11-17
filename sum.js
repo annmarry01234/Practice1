653e03953a1318682159f89f2509079bdec9b365
annmarry01234 <annmaryglarance@gmail.com>
2025-11-17 17:20:41 +0530

    Delete sum.js

--- sum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = add;
-function add(a, b) {
-    return a + b;
-}
-// Add a test call to see output
-console.log("2 + 3 =", add(2, 3));
